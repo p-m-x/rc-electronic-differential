bd74a70ce8c8ccaad536524f1551b5669434c9db
Przemek Talarek <przemyslaw.talarek@kinguin.io>
2019-10-17 20:20:16 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define PW_LEFT_MAX 600
+#define PW_RIGHT_MAX 2400
+#define PW_FORWARD_MAX 2400
+#define PW_BACk_MAX 600
+#define PW_ZERO_TOLERATION 5
+#define PW_AVG_SAMPLE_SIZE 20
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
