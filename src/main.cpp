eb0aee41eef0306d5693d2fa777dbba215747946
Przemek Talarek <przemyslaw.talarek@kinguin.io>
2019-10-19 23:17:42 +0200

    speed calculations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
+#include <BTS7960.h>
 
 #define PW_LEFT_MAX 600
 #define PW_RIGHT_MAX 2400
 #define PW_FORWARD_MAX 2400
-#define PW_BACk_MAX 600
+#define PW_BACKWARD_MAX 600
 #define PW_ZERO_TOLERATION 5
 #define PW_AVG_SAMPLE_SIZE 20
 #define SPEED_DIFF_RATIO 0.2
@@ -21,84 +22,103 @@
 #define MR_EN_R A3
 #define MR_EN_L A4
 
+#define DEBUG 0
+
+#define MOVE_DIR_FORWARDS 0
+#define MOVE_DIR_BACKWARDS 1
+
 unsigned long servoTimeout = 3000;
 unsigned long PWServoThrottle = 0;
 unsigned long PWServoDirection = 0;
 unsigned long directionNeutralPosition = 0;
 unsigned long throttleNeutralPosition = 0;
 
+BTS7960 motorLeft(ML_EN_R, ML_RPWM,  ML_EN_L, ML_LPWM, DEBUG);
+BTS7960 motorRight(MR_EN_R, MR_RPWM, MR_EN_L, MR_LPWM, DEBUG);
+
 void setupHardware();
-void move(int motorLPWM, int motorRPWM, bool forward);
+void move(int motorLeftSpeed, int motorRightSpeed, int dir);
 void stop(boolean freewheel) ;
-float normalizeServoValue(unsigned long servoPW, unsigned long neutralPosition, int scope);
+int normalizeServoValue(unsigned long servoPW, unsigned long neutralPosition, int scope);
+void calculateMotorsSpeed(int &motorLeftSpeed, int &motorRightSpeed, int direction);
 
 void setup() {
   setupHardware();
   directionNeutralPosition = PW_LEFT_MAX + (PW_RIGHT_MAX - PW_LEFT_MAX) / 2;
-  throttleNeutralPosition = PW_RIGHT_MAX + (PW_FORWARD_MAX - PW_BACk_MAX) / 2;
+  throttleNeutralPosition = PW_BACKWARD_MAX + (PW_FORWARD_MAX - PW_BACKWARD_MAX) / 2;
+
+  if (DEBUG) {
+    Serial.print(F("Direction servo neutral RAW value: "));
+    Serial.println(directionNeutralPosition);
+    Serial.print(F("Throttle servo neutral RAW value: "));
+    Serial.println(throttleNeutralPosition);
+  }
 }
 
 void loop() {
   PWServoThrottle = pulseIn(SERVO_THROTLE_PIN, HIGH, servoTimeout);
   PWServoDirection = pulseIn(SERVO_DIRECTION_PIN, HIGH, servoTimeout);
-  float direction = normalizeServoValue(PWServoDirection, directionNeutralPosition, PW_RIGHT_MAX - PW_LEFT_MAX);
-  float throttle = normalizeServoValue(PWServoThrottle, throttleNeutralPosition, PW_FORWARD_MAX - PW_BACk_MAX);
 
+  int direction = normalizeServoValue(PWServoDirection, directionNeutralPosition, PW_RIGHT_MAX - PW_LEFT_MAX);
+  int throttle = normalizeServoValue(PWServoThrottle, throttleNeutralPosition, PW_FORWARD_MAX - PW_BACKWARD_MAX);
+  int motorLeftSpeed = abs(throttle);
+  int motorRightSpeed = abs(throttle);
+  calculateMotorsSpeed(motorLeftSpeed, motorRightSpeed, direction);
+
+  if (DEBUG) {
+    Serial.print("THROTTLE: ");
+    Serial.print(throttle);
+    Serial.print("  DIR: ");
+    Serial.print(direction);
+    Serial.print("  SPEED L: ");
+    Serial.print(motorLeftSpeed);
+    Serial.print("  SPEED R: ");
+    Serial.println(motorRightSpeed);
+  }
+  
+  if (throttle > 0) {    
+    move(motorLeftSpeed, motorRightSpeed, MOVE_DIR_FORWARDS);
+  } else if (throttle < 0) {
+    move(motorLeftSpeed, motorRightSpeed, MOVE_DIR_BACKWARDS);
+  } else {
+    stop(false);
+  }
 }
 
-// move forward = true, move back = false
-void move(int motorLPWM, int motorRPWM, bool forward) {
-  digitalWrite(ML_EN_L, !forward);
-  digitalWrite(ML_EN_R, forward);
-  digitalWrite(MR_EN_L, !forward);
-  digitalWrite(MR_EN_R, forward);
-
-  if (forward) {
-    digitalWrite(ML_LPWM, LOW);
-    analogWrite(ML_RPWM, motorLPWM);
-
-    digitalWrite(MR_LPWM, LOW);
-    analogWrite(MR_RPWM, motorRPWM);
-  } else {
-    digitalWrite(ML_RPWM, LOW);
-    analogWrite(ML_LPWM, motorLPWM);
-
-    digitalWrite(MR_RPWM, LOW);
-    analogWrite(MR_LPWM, motorRPWM);
-  }  
+void move(int motorLeftSpeed, int motorRightSpeed, int dir) {
+  motorLeft.rotate(motorLeftSpeed, dir);
+  motorRight.rotate(motorRightSpeed, dir);
 }
 
 void stop(boolean freewheel) {
-  digitalWrite(ML_EN_L, !freewheel);
-  digitalWrite(ML_EN_R, !freewheel);
-  digitalWrite(MR_EN_L, !freewheel);
-  digitalWrite(MR_EN_R, !freewheel);
-  digitalWrite(ML_LPWM, LOW);
-  digitalWrite(ML_RPWM, LOW);
-  digitalWrite(MR_LPWM, LOW);
-  digitalWrite(MR_RPWM, LOW);
+  motorLeft.stop();
+  motorRight.stop();
 }
 
 void setupHardware() {
   pinMode(SERVO_DIRECTION_PIN, INPUT);
   pinMode(SERVO_THROTLE_PIN, INPUT);
-  pinMode(ML_EN_L, OUTPUT);
-  pinMode(ML_EN_R, OUTPUT);
-  pinMode(MR_EN_L, OUTPUT);
-  pinMode(MR_EN_R, OUTPUT);
-  digitalWrite(ML_EN_L, LOW);
-  digitalWrite(ML_EN_R, LOW);
-  digitalWrite(MR_EN_L, LOW);
-  digitalWrite(MR_EN_R, LOW);
   Serial.begin(9600);
 }
 
-float normalizeServoValue(unsigned long servoPW, unsigned long neutralPosition, int scope) {
+int normalizeServoValue(unsigned long servoPW, unsigned long neutralPosition, int scope) {
 
-  if (servoPW < neutralPosition - PW_ZERO_TOLERATION) {
-    return (servoPW - neutralPosition) / (scope / 2);
-  } else if (servoPW > neutralPosition + PW_ZERO_TOLERATION) {
-    return (servoPW - neutralPosition) / (scope / 2);
+  if ((servoPW < neutralPosition - PW_ZERO_TOLERATION) || (servoPW > neutralPosition + PW_ZERO_TOLERATION)) {
+    long p = servoPW - neutralPosition;
+    return (float(p) / float(scope / 2)) * 100;
   }
-  return 0.0;
+  return 0;
+}
+
+void calculateMotorsSpeed(int &motorLeftSpeed, int &motorRightSpeed, int direction) {
+
+  // turn right
+  if (direction > 0) {
+    motorRightSpeed -= motorRightSpeed * direction/100 * SPEED_DIFF_RATIO;
+  } 
+  // turn left
+  else if (direction < 0) {
+    motorLeftSpeed -= motorLeftSpeed * direction/100 * SPEED_DIFF_RATIO;
+  }
+
 }
